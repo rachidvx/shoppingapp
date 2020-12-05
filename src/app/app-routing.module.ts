e3d1dcdcbd525e8434d25be15725accac168fbc2
Rachid Bouzalmat <rbouzalmat@vxcompany.com>
2020-12-05 17:25:24 +0100

    added implementing http;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
 import {Routes, RouterModule} from '@angular/router';
 import {RecipesComponent} from './recipes/recipes.component'
-import {HomeComponent} from './home/home.component'
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
+import {RecipesResolverService} from './recipes/recipes-resolver.service'
+
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
+
+
 const appRoutes: Routes = [
     {path: "", redirectTo: 'recipes', pathMatch: 'full'},
-    // {path: "", component: HomeComponent},
     {path: "recipes", component: RecipesComponent, children: [
         {path: "", component: RecipeStartComponent},
         {path: "new", component: RecipeEditComponent},
-        {path: ":id", component: RecipeDetailComponent},
-        {path: ":id/edit", component: RecipeEditComponent}
+        {path: ":id", component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+        {path: ":id/edit", component: RecipeEditComponent, resolve: [RecipesResolverService]}
     ]},
     {path: "shoppinglist", component: ShoppingListComponent}
 ];
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,27 +8,34 @@ export class RecipeService {
     
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            "Lasagna", 
-            "test description", 
-            "https://img.taste.com.au/RCqdtLpm/w720-h480-cfill-q80/taste/2019/08/haloumi-lasagne-153157-2.jpg",
-            [
-                new Ingredient("Meat", 10),
-                new Ingredient("Tomatoes", 3)
-            ]
-        ),
-        new Recipe
-        (
-            "Cheescake", 
-            "test description2", 
-            "https://images-gmi-pmc.edge-generalmills.com/6624f074-deb4-4ad2-b2d0-ff2a9839c8a7.jpg",
-            [
-                new Ingredient("Chees", 4),
-                new Ingredient("Succer", 20)
-            ]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         "Lasagna", 
+    //         "test description", 
+    //         "https://img.taste.com.au/RCqdtLpm/w720-h480-cfill-q80/taste/2019/08/haloumi-lasagne-153157-2.jpg",
+    //         [
+    //             new Ingredient("Meat", 10),
+    //             new Ingredient("Tomatoes", 3)
+    //         ]
+    //     ),
+    //     new Recipe
+    //     (
+    //         "Cheescake", 
+    //         "test description2", 
+    //         "https://images-gmi-pmc.edge-generalmills.com/6624f074-deb4-4ad2-b2d0-ff2a9839c8a7.jpg",
+    //         [
+    //             new Ingredient("Chees", 4),
+    //             new Ingredient("Succer", 20)
+    //         ]
+    //     )
+    // ];
+
+    private recipes: Recipe[] = [];
+
+    setRecipes(recipes: Recipe []) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }    
 
     getRecipes() : Recipe []{
         return this.recipes.slice();
